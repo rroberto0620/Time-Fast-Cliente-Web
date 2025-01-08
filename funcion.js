6ac932e77312a8200084bd9e0201f0638d759e00
Roberto <zS21016329@estudiantes.uv.mx>
2025-01-08 03:19:29 -0600

    cliente web cambios

--- funcion.js
+++ funcion.js
@@ -2,6 +2,8 @@ const URL_WS = 'http://localhost:8084/Time-Fast/api/envio/obtenerEnvioNoGuia/';
 const URL_WS_Paquetes = 'http://localhost:8084/Time-Fast/api/paquete/obtenerPaquetesEnvio/';
 const URL_WS_Historial = 'http://localhost:8084/Time-Fast/api/posee/obtenerHistorialPorIdEnvio/';
 const containerInfo = document.getElementById('container-info');
+const containerInfo2 = document.getElementById('container-info2');
+const errorMessage = document.getElementById('error-message');
 
 async function obtenerHistorial(idEnvio){
     //INICIA CONSUMO WS
@@ -14,19 +16,25 @@ async function obtenerHistorial(idEnvio){
         if(respuesta.ok){
                 const historiales = await respuesta.json();
                 console.log(historiales);
-                historiales.forEach(historial =>{
-                    const paqueteElemento2 = document.createElement('div');
-                    paqueteElemento2.className = 'container-products';
-                    paqueteElemento2.innerHTML = `
-                        <div class="products"> 
-                            <ul>
-                                <li><strong>Motivo:${historial.motivo}  Estatus:${historial.estatus}  Hora y Fecha:${historial.tiempo}</strong></li>
-                            </ul>
-                        </div>
-                    `;
-                    containerInfo.appendChild(paqueteElemento2);
-                });
+                let htmlContent = `
+        <div id="historial" class="container">
+            <h2>Historial de cambios</h2>
+            <div id="historial2" class="timeline"> 
+    `;
 
+    historiales.forEach(historial => {
+        htmlContent += `
+            <ul>
+                <li><strong>Motivo:  </strong>${historial.motivo}</li>
+                <li><strong>Estatus:  </strong>${historial.estatus}</li>
+                <li><strong>Hora y Fecha:  </strong>${historial.tiempo}</li>
+            </ul>
+            <hr>
+        `;
+    });
+    htmlContent += `<div/></div>`;
+    containerInfo2.innerHTML = htmlContent;
+    containerInfo.appendChild(containerInfo2)
         }else{
             throw new Error(`Error en la petición: ${respuesta.status}`)
         }
@@ -46,6 +54,7 @@ async function obtenerPaquetesEnvio(idEnvio){
 
         if(respuesta.ok){
                 const paquetes = await respuesta.json();
+                const headerProduct = document.getElementById('headerProduct');
                 console.log(paquetes);
                 paquetes.forEach(paquete =>{
                     const paqueteElemento = document.createElement('div');
@@ -59,7 +68,7 @@ async function obtenerPaquetesEnvio(idEnvio){
                             </ul>
                         </div>
                     `;
-                    containerInfo.appendChild(paqueteElemento);
+                    headerProduct.appendChild(paqueteElemento);
                 });
 
         }else{
@@ -81,61 +90,75 @@ async function obtenerInformacionEnvio(){
 
         });
 
+        if (!respuesta.ok) {
+            throw new Error(`Error en la petición: ${respuesta.status}`);
+        }
+
+        const data = await respuesta.json();
+
+        if (data.mensaje === "Numero de guia incorrecto") {
+                alert('Este numero de guia no existe, por favor intente con otro.');
+            return;
+        }
+
         if(respuesta.ok){
-                const data = await respuesta.json();
+            errorMessage.style.display = 'none';
                 console.log(data.envio);
                 const idEnvio = data.envio.id;
                 console.log(idEnvio);
-                obtenerPaquetesEnvio(idEnvio)
-                obtenerHistorial(idEnvio)
                 mostrarInformacion(containerInfo,data);
+                obtenerHistorial(idEnvio)
+                obtenerPaquetesEnvio(idEnvio)
                 window.scrollTo({
                     top: document.body.scrollHeight,
                     behavior: 'smooth'
                 });
-        }else{
+            } 
+        else{
             throw new Error(`Error en la petición: ${respuesta.status}`)
         }
     } catch(error){
         console.error('Error en la peticion ',error);
+        containerInfo.innerHTML = '<p>Lo sentimos, hubo un error al consultar la información.</p>'
     }
 }
 
-
-function mostrarInformacion(containerInfo,data){
-    containerInfo.innerHTML =`<div class="container">
-    <div class="content">
-        <h2>Estatus del Envío</h2>
-        <p><strong>Envío con número de guía:</strong> <span>${data.envio.numeroGuia}</span></p>
-        <p class="status"><strong>Estado:</strong> <span id="currentStatus">${data.envio.estatus}</span></p>
-        <div class="info-container">
-            <div class="column">
-                <h3>Origen</h3>
-                <p><strong>Dirección:</strong> ${data.envio.calleOrigen} ${data.envio.numeroOrigen} Col. ${data.envio.coloniaOrigen}</p>
-                <p><strong>Código Postal:</strong> ${data.envio.cpOrigen}</p>
-                <p><strong>Ciudad:</strong> ${data.envio.ciudadOrigen}</p>
-                <p><strong>Estado:</strong> ${data.envio.estadoOrigen}</p>
-            </div>
-            <div class="column">
-                <h3>Destino</h3>
-                <p><strong>Dirección:</strong> ${data.envio.calleDestino} ${data.envio.numeroDestino} Col. ${data.envio.coloniaDestino}</p>
-                <p><strong>Código Postal:</strong> ${data.envio.cpDestino}</p>
-                <p><strong>Ciudad:</strong> ${data.envio.ciudadDestino}</p>
-                <p><strong>Estado:</strong> ${data.envio.estadoDestino}</p>
+function mostrarInformacion(containerInfo, data) {
+    containerInfo.innerHTML = `
+        <div class="container">
+            <div class="content">
+                <h2>Estatus del Envío</h2>
+                <p><strong>Envío con número de guía:</strong> <span>${data.envio.numeroGuia}</span></p>
+                <p class="status"><strong>Estado:</strong> <span id="currentStatus">${data.envio.estatus}</span></p>
+                <div class="info-container">
+                    <div class="column">
+                        <h3>Origen</h3>
+                        <p><strong>Dirección:</strong> ${data.envio.calleOrigen} ${data.envio.numeroOrigen} Col. ${data.envio.coloniaOrigen}</p>
+                        <p><strong>Código Postal:</strong> ${data.envio.cpOrigen}</p>
+                        <p><strong>Ciudad:</strong> ${data.envio.ciudadOrigen}</p>
+                        <p><strong>Estado:</strong> ${data.envio.estadoOrigen}</p>
+                        <button style="background-color: transparent; display: block; margin: 0 auto;" onclick="abrirEnMaps('${data.envio.calleDestino} ${data.envio.numeroDestino}, ${data.envio.coloniaDestino}, ${data.envio.ciudadDestino}, ${data.envio.estadoDestino}')">
+                            <a href="#"><img src="google-maps-svgrepo-com.svg" alt="Maps" width="50"></a>
+                        </button>
+                    </div>
+                    <div class="column">
+                        <h3>Destino</h3>
+                        <p><strong>Dirección:</strong> ${data.envio.calleDestino} ${data.envio.numeroDestino} Col. ${data.envio.coloniaDestino}</p>
+                        <p><strong>Código Postal:</strong> ${data.envio.cpDestino}</p>
+                        <button style="background-color: transparent; display: block; margin: 0 auto;" onclick="abrirEnMaps('${data.envio.calleDestino} ${data.envio.numeroDestino}, ${data.envio.coloniaDestino}, ${data.envio.ciudadDestino}, ${data.envio.estadoDestino}')">
+                            <a href="#"><img src="google-maps-svgrepo-com.svg" alt="Maps" width="50"></a>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="timeline">
-            <h3>Historial de Cambios</h3>
-            <ul>
-                <li><strong>10:50 a.m.:</strong> Delimitado (Xalapa, Ver)</li>
-                <li><strong>12:00 p.m.:</strong> En tránsito</li>
-                <li><strong>10:30 p.m.:</strong> Finalizado</li>
-            </ul>
-        </div>
-    </div>
-</div>
-<h3>Productos contenidos</h3>`;
+        <h2 id="headerProduct">Productos contenidos</h2>`;
     document.body.appendChild(containerInfo);
+}
+
+function abrirEnMaps(direccion) {
+    const direccionCodificada = encodeURIComponent(direccion); 
+    window.open(`https://www.google.com/maps/search/?api=1&query=${direccionCodificada}`, '_blank'); 
 }
 
 window.addEventListener('scroll', () => {
@@ -155,15 +178,11 @@ document.getElementById('backToTop').addEventListener('click', () => {
 });
 
 document.getElementById('trackingNumber').addEventListener('input', function (e) {
-    // Permitir solo números en el input
     this.value = this.value.replace(/[^0-9]/g, '');
 });
 
 document.getElementById('rastrear').addEventListener('click', function () {
     const input = document.getElementById('trackingNumber');
-    const errorMessage = document.getElementById('error-message');
-    
-    // Validar si el input está vacío o no tiene 6 caracteres
     if (input.value.trim() === '') {
         errorMessage.textContent = 'El campo no puede estar vacío.';
         errorMessage.style.display = 'inline';
@@ -173,4 +192,13 @@ document.getElementById('rastrear').addEventListener('click', function () {
     } else {
         errorMessage.style.display = 'none';
     }
+});
+
+document.getElementById('borrarButton').addEventListener('click', function() {
+    // Selecciona el input y borra su contenido
+    document.getElementById('trackingNumber').value = '';
+});
+
+document.getElementById('headTime').addEventListener('click', function() {
+    location.reload(); // Recarga la página
 });
